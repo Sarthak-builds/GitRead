476de944600a5988b9e01461b731d049fa972c86
Sarthak-builds <sarthakshiroty04@gmail.com>
2025-07-19 01:21:29 +0530

    completed the responsiveness

--- src/components/Dashboard/Screenshot.jsx
+++ src/components/Dashboard/Screenshot.jsx
@@ -8,13 +8,13 @@ if (!data || !data.lighthouseResult || !data.lighthouseResult.categories) {
   }
 
     return (
-        <div className="flex gap-3 w-full px-40 h-80 pt-10 items-center">
+        <div className="flex gap-3 w-full px-4 sm:px-15 md:px-25 lg:px-40 xl:px-40 h-fit flex-col lg:flex-row pt-10 items-center justify-center ">
         <ScoreCard category="Performance" scoreData={data.lighthouseResult.categories.performance.score*100}
         about="The Performance score assesses the overall speed and responsiveness of your webpage. It measures how quickly the content appears on the screen, how fast it becomes usable, and how smoothly it responds to user interactions. Essentially, it reflects the user's perceived loading experience."
         goodScore="90-100"
 
         ></ScoreCard>
-          <div className="w-fit h-full overflow-hidden shadow-md shadow-green-500/20 rounded-lg">
+          <div className="w-fit h-50 sm:h-70 overflow-hidden shadow-md shadow-green-500/20 rounded-lg">
              <img 
             src={`${data.lighthouseResult.fullPageScreenshot?.screenshot?.data}`} 
             alt="Website Screenshot" 
