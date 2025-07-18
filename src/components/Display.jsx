f64aab2c2e5ed79c281ab9b9bf49cdc3098422af
Sarthak-builds <sarthakshiroty04@gmail.com>
2025-07-19 01:50:00 +0530

    test 2 for the build error in display section

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,9 +1,9 @@
 import MetricHeading from "./Dashboard/MertricHeading";
-import Screenshot from "./src/components/Dashboard/Screenshot.jsx";
 import ScoreBoard from "./Dashboard/ScoreBoard";
 import WebVitals from "./Dashboard/WebVitals";
 import LabVitals from "./Dashboard/LabVitals";
 import {motion} from 'motion/react'
+import Screenshot from "./Dashboard/screenshot";
 function Display() {
 
 
@@ -11,7 +11,7 @@ function Display() {
     return (
        <>
          <MetricHeading heading="Overall Health Scorecard" description=" The 'Overall Health Scorecard' serves as the executive summary of your website's performance and quality. It provides an immediate, high-level overview, allowing developers and stakeholders to grasp the general health of the page at a single glance without diving into granular details."></MetricHeading>
-     <Screenshot></Screenshot>
+    <Screenshot></Screenshot>
      <ScoreBoard></ScoreBoard>
      <MetricHeading heading="Core Web Vitals Status" description="This section focuses on Google's most important user-centric performance metrics, which are now key ranking signals. It's designed to give a precise understanding of the page's loading speed, interactivity, and visual stability from both real users and simulated tests."></MetricHeading>
      <WebVitals></WebVitals>
