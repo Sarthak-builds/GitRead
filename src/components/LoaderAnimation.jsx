0523cd93ef90ce215fb6e5b96eb1f5784eef6b18
Sarthak-builds <sarthakshiroty04@gmail.com>
2025-07-19 21:04:27 +0530

    changes in loader

--- src/components/LoaderAnimation.jsx
+++ src/components/LoaderAnimation.jsx
@@ -4,9 +4,9 @@ function LoaderAnimation() {
 
     return (
         <div className="flex space-x-3 items-center justify-center animate-bounce">
-      <div className="h-4 w-4 sm:h-5 sm:w-5 md:h-7 md:w-7 bg-green-600 rounded-full animate-bounce"></div>
-      <div className="h-3 w-3 sm:h-4 sm:w-4 md:h-5 md:w-5 bg-green-600 rounded-full animate-bounce" style={{animationDelay: '0.1s'}}></div>
-      <div className="h-4 w-4 sm:h-5 sm:w-5 md:h-7 md:w-7 bg-green-600 rounded-full animate-bounce" style={{animationDelay: '0.2s'}}></div>
+      <div className="h-2 w-2 sm:h-3 sm:w-3 md:h-4 md:w-4 bg-green-600 rounded-full animate-bounce"></div>
+      <div className="h-2 w-2 sm:h-3 sm:w-3 md:h-4 md:w-4 bg-green-600 rounded-full animate-bounce" style={{animationDelay: '0.1s'}}></div>
+      <div className="h-2 w-2 sm:h-3 sm:w-3 md:h-4 md:w-4 bg-green-600 rounded-full animate-bounce" style={{animationDelay: '0.2s'}}></div>
     </div>
     )
 }
